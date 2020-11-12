ca8d177f60863abfe5a95950ff825117be053afc
Daniel Kuroski <daniel.kuroski@gmail.com>
2020-11-12 21:55:44 +0100

    First commit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Daniel Kuroski on 12.11.20.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
