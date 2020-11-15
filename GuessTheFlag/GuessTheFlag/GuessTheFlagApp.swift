53e35d5a327297cf71b4c49407e8b5bf6a774e65
Daniel Kuroski <daniel.kuroski@gmail.com>
2020-11-15 19:07:02 +0100

    Finish GuessTheFlag app

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Daniel Kuroski on 15.11.20.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
