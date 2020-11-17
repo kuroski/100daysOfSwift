cffde3f974386cdbf95c9938632a300f9bebd694
Daniel Kuroski <daniel.kuroski@gmail.com>
2020-11-17 22:07:01 +0100

    Fix game logic

--- RockPaperScissors/RockPaperScissors/ContentView.swift
+++ RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -12,25 +12,25 @@ enum Move: String, CaseIterable {
     case paper = "Paper"
     case scissors = "Scissors"
     
-    var howToWin: String {
+    var winDescription: String {
         switch self {
         case .rock:
-            return "Rocks crush scissors."
+            return "🪨 Rocks crush scissors."
         case .paper:
-            return "Paper covers rock."
+            return "📃 Paper covers rock."
         case .scissors:
-            return "Scissors cut paper."
+            return "✂️ Scissors cut paper."
         }
     }
     
-    var howToLose: String {
+    var loseDescription: String {
         switch self {
         case .rock:
-            return "Paper covers rock."
+            return "📃 Paper covers rock."
         case .paper:
-            return "Scissors cut paper."
+            return "✂️ Scissors cut paper."
         case .scissors:
-            return "Rocks crush scissors."
+            return "🪨 Rocks crush scissors."
         }
     }
     
@@ -48,6 +48,18 @@ enum Move: String, CaseIterable {
     func does(loseAgainst opponentMove: Move) -> Bool {
         return self != opponentMove && opponentMove.does(winAgainst: self)
     }
+    
+    static func who(winAgainst opponentMove: Move) -> Move {
+        return Move.allCases.first { (move) -> Bool in
+            return move.does(winAgainst: opponentMove)
+        }!
+    }
+    
+    static func who(loseAgainst opponentMove: Move) -> Move {
+        return Move.allCases.first { (move) -> Bool in
+            return move.does(loseAgainst: opponentMove)
+        }!
+    }
 }
 
 struct Game: Identifiable {
@@ -63,11 +75,12 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Text(opponentMove.rawValue)
+            Text(opponentMove.rawValue).font(.title)
             
             Text(shouldWin ? "Choose to win" : "Choose to lose")
+                .padding(.vertical, 10)
             
-            HStack {
+            HStack(spacing: 30) {
                 ForEach(Move.allCases, id: \.self) { move in
                     Button(move.rawValue, action: {
                         choice = Game(move: move)
@@ -77,7 +90,15 @@ struct ContentView: View {
         }.alert(item: $choice) { selectedChoice in
             let gotItRight = shouldWin ? selectedChoice.move.does(winAgainst: opponentMove) : selectedChoice.move.does(loseAgainst: opponentMove)
             
-            return Alert(title: Text(gotItRight ? "You win!" : "You lose!"), message: Text(shouldWin ? selectedChoice.move.howToWin : selectedChoice.move.howToLose), dismissButton: Alert.Button.default(Text("Continue"), action: {
+            print([shouldWin, gotItRight, selectedChoice.move.rawValue, opponentMove.rawValue])
+            
+            var message = shouldWin ? selectedChoice.move.winDescription : selectedChoice.move.loseDescription
+            
+            if !gotItRight {
+                message = shouldWin ? Move.who(winAgainst: opponentMove).winDescription : Move.who(loseAgainst: opponentMove).loseDescription
+            }
+            
+            return Alert(title: Text(gotItRight ? "Correct!" : "Wrong!"), message: Text(message), dismissButton: Alert.Button.default(Text("Continue"), action: {
                 choice = nil
                 shouldWin = Bool.random()
                 opponentMove = Move.allCases.randomElement()!
