1890cc721b36acc2797fed78b2d53a1b1d94689f
Daniel Kuroski <daniel.kuroski@gmail.com>
2020-11-19 22:01:42 +0100

    Finish project 5 Word Scramble

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Daniel Kuroski on 19.11.20.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
