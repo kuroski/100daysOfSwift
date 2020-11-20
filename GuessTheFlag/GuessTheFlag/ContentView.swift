fcf1504873c8d3a5644077a363c3be8b75e57f53
Daniel Kuroski <daniel.kuroski@gmail.com>
2020-11-20 14:51:52 +0100

    Finish day 34 challenges

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -15,6 +15,9 @@ struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     
+    @State private var rotationDegrees = 0.0
+    @State private var dimWrongFlags = false
+    
     var body: some View {
         ZStack {
             LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
@@ -28,13 +31,23 @@ struct ContentView: View {
                 .font(.largeTitle)
                 
                 ForEach(0 ..< 3) { number in
-                    Button(action: { flagTapped(number) }) {
+                    Button(action: {
+                        withAnimation(.spring()) {
+                            self.rotationDegrees += 360.0
+                            self.dimWrongFlags = true
+                        }
+                        
+                        flagTapped(number)
+                    }) {
                         Image(self.countries[number])
                             .renderingMode(.original)
                             .clipShape(Capsule())
                             .overlay(Capsule().stroke(Color.black, lineWidth: 1))
                             .shadow(color: .black, radius: 2)
                     }
+                    .rotation3DEffect(
+                        .degrees(number == self.correctAnswer ? self.rotationDegrees : 0), axis: (x: 0, y: 1, z: 0))
+                    .opacity(self.dimWrongFlags && number != self.correctAnswer ? 0.25 : 1.0)
                 }
                 
                 Spacer()
@@ -60,10 +73,15 @@ struct ContentView: View {
             scoreTitle = "Wrong! That’s the flag of \(countries[number])"
         }
         
-        showingScore = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            showingScore = true
+        }
     }
     
     func askQuestion() {
+        rotationDegrees = 0.0
+        dimWrongFlags = false
+        
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
     }
