e171db53e7f7cdb4dbcb7c60321637327a96c6dd
Daniel Kuroski <daniel.kuroski@gmail.com>
2020-11-23 08:59:38 +0100

    Start improving application layout

--- MultiplicationTables/MultiplicationTables/ContentView.swift
+++ MultiplicationTables/MultiplicationTables/ContentView.swift
@@ -7,6 +7,51 @@
 
 import SwiftUI
 
+struct Shake: GeometryEffect {
+    var amount: CGFloat = 10
+    var shakesPerUnit = 3
+    var animatableData: CGFloat
+    
+    func effectValue(size: CGSize) -> ProjectionTransform {
+        ProjectionTransform(CGAffineTransform(translationX:
+                                                amount * sin(animatableData * .pi * CGFloat(shakesPerUnit)),
+                                              y: 0))
+    }
+}
+
+enum Animals: String, CaseIterable {
+    case bear
+    case buffalo
+    case chick
+    case chicken
+    case cow
+    case crocodile
+    case dog
+    case duck
+    case elephant
+    case frog
+    case giraffe
+    case goat
+    case gorilla
+    case hippo
+    case horse
+    case monkey
+    case moose
+    case narwhal
+    case owl
+    case panda
+    case parrot
+    case penguin
+    case pig
+    case rabbit
+    case rhino
+    case sloth
+    case snake
+    case walrus
+    case whale
+    case zebra
+}
+
 struct Question: Identifiable {
     let id = UUID()
     let title: String
@@ -25,15 +70,43 @@ struct SettingsView: View {
     
     var generateQuestions: (Int, Int) -> Void
     
+    var animals: [Animals] = Array(Animals.allCases.shuffled()[..<12])
+    
+    let animalColumns: [GridItem] = Array(repeating: .init(.flexible()), count: 4)
+    
     var body: some View {
         Form {
             Section(header: Text("Multiplication table")) {
-                Stepper(value: $table, in: 1...12, onEditingChanged: { val in
-                    if table == 1 && amount == 20 {
-                        amount = 10
+                HStack {
+                    Spacer()
+                    VStack {
+                        Stepper(String(table), value: $table, in: 1...12, onEditingChanged: { val in
+                            if table == 1 && amount == 20 {
+                                amount = 10
+                            }
+                        })
+                        .labelsHidden()
+                        .padding(.bottom, 20)
+                        
+                        LazyVGrid(columns: animalColumns, spacing: 16) {
+                            ForEach((0...(table - 1)), id: \.self) { index in
+                                ZStack(alignment: .topLeading) {
+                                    Image(animals[index].rawValue).resizable().scaledToFit().frame(height: 50)
+                                    Text(String(index+1))
+                                        .font(.body)
+                                        .padding(6)
+                                        .background(Color.white)
+                                        .clipShape(Circle())
+                                        .overlay(
+                                            Circle()
+                                                .stroke(Color.gray, style: StrokeStyle(lineWidth: 2))
+                                        )
+                                        .offset(x: -10, y: -15)
+                                }
+                            }
+                        }.animation(.spring())
                     }
-                }) {
-                    Text(String(table))
+                    Spacer()
                 }
             }
             
@@ -63,7 +136,6 @@ struct SettingsView: View {
                         Spacer()
                     }
                 }
-                .frame(height: .infinity)
                 .contentShape(Capsule())
                 .foregroundColor(Color.white)
                 .listRowBackground(Color.purple)
@@ -76,35 +148,67 @@ struct SettingsView: View {
 struct GameView: View {
     @State private var currentQuestion = 0
     @State private var answer = ""
+    @State private var hasError = false
     
     var questions: [Question]
     var questionAnswered: (String, Int) -> Void
     
     var body: some View {
-        Form {
-            Section {
-                Text(questions[currentQuestion].title)
-            }
+        VStack {
+            Spacer()
             
-            Section {
-                TextField("Answer", text: $answer)
-                    .keyboardType(.numberPad)
-            }
-            
-            Section {
-                Button("Submit", action: {
+            VStack {
+                HStack(spacing: 0) {
+                    Text("\(questions[currentQuestion].title) = ")
+                        .font(.system(size: 50))
+                        .fontWeight(.bold)
+                    
+                    TextField("?", text: $answer)
+                        .keyboardType(.numberPad)
+                        .padding(.vertical, 10)
+                        .lineLimit(1)
+                        .multilineTextAlignment(.center)
+                        .font(.system(size: 50))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 6)
+                                .stroke(hasError ? Color.red.opacity(0.8) : Color.purple.opacity(0.8), lineWidth: 3)
+                        )
+                        .frame(maxWidth: 100)
+                        .modifier(Shake(animatableData: CGFloat(hasError ? 1.0 : 0.0)))
+                }.padding(.bottom, 50)
+                
+                Button(action: {
+                    if (answer.isEmpty) {
+                        withAnimation(.default) {
+                            self.hasError = true
+                        }
+                        return
+                    }
+                    
                     self.questionAnswered(answer, currentQuestion)
+                    self.hasError = false
                     
                     if self.currentQuestion + 1 < questions.count {
                         self.currentQuestion += 1
                     }
                     
                     self.answer = ""
-                })
-            }
+                }) {
+                    Image(systemName: "checkmark.seal")
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                }
+                .rotation3DEffect(.degrees(hasError ? 360.0 : 0.0), axis: (x: 0, y: 0, z: 1))
+                .foregroundColor(.white)
+                .colorMultiply(hasError ? .red : .blue)
+                .animation(Animation.easeInOut(duration: 1).delay(0.3))
+            }.padding()
+            
+            Spacer()
             
             ProgressView(value: Double(currentQuestion + 1), total: Double(questions.count))
         }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
@@ -136,11 +240,16 @@ struct ContentView: View {
                 GameView(questions: questions, questionAnswered: questionAnswered)
             }
         }
+        .onTapGesture {
+            self.hideKeyboard()
+        }
+        .onAppear {
+            self.generateQuestions(withTable: 1, withAmount: 10)
+        }
     }
     
     private func questionAnswered(_ answer: String, forQuestion index: Int) -> Void {
         self.questions[index].answerQuestion(answer)
-        
     }
     
     private func generateQuestions(withTable table: Int, withAmount amount: Int) -> Void {
